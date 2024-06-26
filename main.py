2ddaf32f47f8154e81636613ce4182a28764dc5b
Aviral Srivastava <82719498+aviral-sri@users.noreply.github.com>
2024-06-26 13:06:47 +0530

    Timestamp And Streak
     Workout time is 2 sec (line 85)

--- main.py
+++ main.py
@@ -1,236 +1,150 @@
 import random
-from gtts import gTTS
 import time
-'''this project is under construction,
-you can generate a workout plan according to you prefrence...
-but for now you cant save it or use it while working out...
-'''
+from datetime import datetime
 
 
 def plan_workout():
-
     def user_preffn():
-        user_age = int(input("Your Age::"))
-        user_height = int(input("Your Height(in cm)::"))
-        user_weight = int(input("Your Weight(in kg)::"))
-        user_gender = input("Your Gender(M/F)::")
-        user_bmi = user_weight / (user_height / 100 * user_height / 100)
-        user_pref = {
-            "user_age": user_age,
-            "user_height": user_height,
-            "user_weight": user_weight,
-            "user_gender": user_gender,
-            "user_bmi": user_bmi
-        }
-        return user_pref
+        try:
+            user_age = int(input("Your Age: "))
+            user_height = int(input("Your Height (in cm): "))
+            user_weight = int(input("Your Weight (in kg): "))
+            user_gender = input("Your Gender (M/F): ").upper()
+            user_bmi = user_weight / ((user_height / 100) ** 2)
+            user_pref = {
+                "user_age": user_age,
+                "user_height": user_height,
+                "user_weight": user_weight,
+                "user_gender": user_gender,
+                "user_bmi": user_bmi
+            }
+            return user_pref
+        except ValueError:
+            print("Please enter valid inputs.")
+            return None
 
-    def user_is_male_undewight():
-        if user_pref["user_age"] >= 18:
-            from MaleWorkouts import underweight_male_above18_exercises
-            selected_exercises = random.sample(
-                list(underweight_male_above18_exercises.keys()), 10)
-            print("Here is your workout Plan:")
-            exercise_string = "\n".join(selected_exercises)
-            print(exercise_string)
-            return exercise_string
-
-        elif user_pref["user_age"] < 18:
-            from MaleWorkouts import underweight_male_under_18_exercises
-            selected_exercises = random.sample(
-                list(underweight_male_under_18_exercises.keys()), 8)
-            print("Here is your workout Plan:")
-            exercise_string = "\n".join(selected_exercises)
-            print(exercise_string)
-            return exercise_string
-
-        else:
-            print("either there was error in code or your input is wrong")
-
-    def user_is_male_normalweight():
-        if user_pref["user_age"] >= 18:
-            from MaleWorkouts import normal_weight_over_18_exercises
-            selected_exercises = random.sample(
-                list(normal_weight_over_18_exercises.keys()), 14)
-            print("Here is your workout Plan:")
-            exercise_string = "\n".join(selected_exercises)
-            print(exercise_string)
-            return exercise_string
-
-        elif user_pref["user_age"] < 18:
-            from MaleWorkouts import normal_weight_under_18_exercises
-            selected_exercises = random.sample(
-                list(normal_weight_under_18_exercises.keys()), 10)
-            print("Here is your workout Plan:")
-            exercise_string = "\n".join(selected_exercises)
-            print(exercise_string)
-            return exercise_string
-
-        else:
-            print("either there was error in code or your input is wrong")
-
-    def user_is_male_overweight():
-        if user_pref["user_age"] >= 18:
-            from MaleWorkouts import overweight_over_18_exercises
-            selected_exercises = random.sample(
-                list(overweight_over_18_exercises.keys()), 13)
-            print("Here is your workout Plan:")
-            exercise_string = "\n".join(selected_exercises)
-            print(exercise_string)
-            return exercise_string
-
-        elif user_pref["user_age"] < 18:
-            from MaleWorkouts import overweight_under_18_exercises
-            selected_exercises = random.sample(
-                list(overweight_under_18_exercises.keys()), 10)
-            print("Here is your workout Plan:")
-            exercise_string = "\n".join(selected_exercises)
-            print(exercise_string)
-            return exercise_string
-
-        else:
-            print("either there was error in code or your input is wrong")
-
-    def user_is_female_undewight():
-        from FemaleWorkouts import underweight_female_exercises
-        if user_pref["user_age"] >= 18:
-            selected_exercises = random.sample(
-                list(underweight_female_exercises.keys()), 10)
-            print("Here is your workout Plan:")
-            exercise_string = "\n".join(selected_exercises)
-            print(exercise_string)
-            return exercise_string
-
-        elif user_pref["user_age"] < 18:
-            selected_exercises = random.sample(
-                list(underweight_female_exercises.keys()), 8)
-            print("Here is your workout Plan:")
-            exercise_string = "\n".join(selected_exercises)
-            print(exercise_string)
-            return exercise_string
-
-        else:
-            print("either there was error in code or your input is wrong")
-
-    def user_is_female_normalweight():
-        from FemaleWorkouts import normal_weight_female_exercises
-        if user_pref["user_age"] >= 18:
-            selected_exercises = random.sample(
-                list(normal_weight_female_exercises.keys()), 14)
-            print("Here is your workout Plan:")
-            exercise_string = "\n".join(selected_exercises)
-            print(exercise_string)
-            return exercise_string
-
-        elif user_pref["user_age"] < 18:
-            selected_exercises = random.sample(
-                list(normal_weight_female_exercises.keys()), 10)
-            print("Here is your workout Plan:")
-            exercise_string = "\n".join(selected_exercises)
-            print(exercise_string)
-            return exercise_string
-
-        else:
-            print("either there was error in code or your input is wrong")
-
-    def user_is_female_overweight():
-        from FemaleWorkouts import overweight_female_exercises
-        if user_pref["user_age"] >= 18:
-            selected_exercises = random.sample(
-                list(overweight_female_exercises.keys()), 13)
-            print("Here is your workout Plan:")
-            exercise_string = "\n".join(selected_exercises)
-            print(exercise_string)
-            return exercise_string
-
-        elif user_pref["user_age"] < 18:
-            selected_exercises = random.sample(
-                list(overweight_female_exercises.keys()), 10)
-            print("Here is your workout Plan:")
-            exercise_string = "\n".join(selected_exercises)
-            print(exercise_string)
-            return exercise_string
-
-        else:
-            print("either there was error in code or your input is wrong")
+    def generate_exercise_plan(workout_dict, num_exercises):
+        selected_exercises = random.sample(list(workout_dict.keys()), num_exercises)
+        print("Here is your workout Plan:")
+        exercise_string = "\n".join(selected_exercises)
+        print(exercise_string)
+        return exercise_string
 
     user_pref = user_preffn()
+    if not user_pref:
+        return None
+
+    print(f"Your BMI is: {user_pref['user_bmi']:.2f}")
+
     if user_pref["user_gender"] == "M":
-        print(user_pref["user_bmi"])
         if user_pref["user_bmi"] < 18.5:
-            print("--------------------")
             print("You are underweight")
-            return user_is_male_undewight()
-        elif 18.5 < user_pref["user_bmi"] < 24.9:
-            print("--------------------")
+            if user_pref["user_age"] >= 18:
+                from MaleWorkouts import underweight_male_above18_exercises as exercises
+            else:
+                from MaleWorkouts import underweight_male_under_18_exercises as exercises
+        elif 18.5 <= user_pref["user_bmi"] < 24.9:
             print("You are normal weight")
-            return user_is_male_normalweight()
-        elif user_pref["user_bmi"] > 24.9:
-            print("--------------------")
-            print("You are overweight")
-            return user_is_male_overweight()
+            if user_pref["user_age"] >= 18:
+                from MaleWorkouts import normal_weight_over_18_exercises as exercises
+            else:
+                from MaleWorkouts import normal_weight_under_18_exercises as exercises
         else:
-            print("error")
-
-    if user_pref["user_gender"] == "F":
+            print("You are overweight")
+            if user_pref["user_age"] >= 18:
+                from MaleWorkouts import overweight_over_18_exercises as exercises
+            else:
+                from MaleWorkouts import overweight_under_18_exercises as exercises
+    elif user_pref["user_gender"] == "F":
         if user_pref["user_bmi"] < 18.5:
-            print("--------------------")
             print("You are underweight")
-            return user_is_female_undewight()
-        elif 18.5 < user_pref["user_bmi"] < 24.9:
-            print("--------------------")
+            from FemaleWorkouts import underweight_female_exercises as exercises
+        elif 18.5 <= user_pref["user_bmi"] < 24.9:
             print("You are normal weight")
-            return user_is_female_normalweight()
-        elif user_pref["user_bmi"] > 24.9:
-            print("--------------------")
-            print("You are overweight")
-            return user_is_female_overweight()
+            from FemaleWorkouts import normal_weight_female_exercises as exercises
         else:
-            print("either there was error in code or your input is wrong")
+            print("You are overweight")
+            from FemaleWorkouts import overweight_female_exercises as exercises
+    else:
+        print("Invalid gender input.")
+        return None
 
+    num_exercises = 10 if user_pref["user_age"] >= 18 else 8
+    return generate_exercise_plan(exercises, num_exercises)
 
-#this runs after 2nd option
 def start_workout():
     print("--------------------")
-    with open("myworkout.txt", "r") as file:
-        work_list = file.read()
-        exercise_list = work_list.split("\n")
+    try:
+        with open("myworkout.txt", "r") as file:
+            work_list = file.read()
+            exercise_list = work_list.split("\n")
 
-    def print_list_with_delay(lst):
-        #this funtion is made with the help of chat gpt
-        for item in lst:
-            print(item)
-            for i in range(30, 0, -1):
-                print(i, end=' ', flush=True)
-                time.sleep(1)  # Wait for 1 second for countdown
-            print()  # Print a newline after countdown
+        def print_list_with_delay(lst):
+            for item in lst:
+                print(item)
+                for i in range(2, 0, -1):
+                    print(i, end=' ', flush=True)
+                    time.sleep(1)
+                print()
 
-        print("Done!")
+            print("Done!")
 
-    print_list_with_delay(exercise_list)
+        print_list_with_delay(exercise_list)
+    except FileNotFoundError:
+        print("Workout plan not found. Please generate a workout plan first.")
 
+timelog = []
+with open("streak.txt", "r") as file:
+    streak = file.read()
+streak = int(streak)
 
 while True:
-    print("\nWorkout Planner Menu:")
+    print("Workout Planner Menu:")
     print("1. Generate Workout Plan")
     print("2. Start Workout")
-    print("3. Display Workout Schedule")
+    print("3. View Log")
     print("4. Exit")
 
-    user_input = int(input("Please select one option::"))
+    timestamp = str(datetime.now().date())
+
+    try:
+        user_input = int(input("Please select one option: "))
+    except ValueError:
+        print("Invalid input. Please select a valid option.")
+        continue
+
     if user_input == 1:
-        print(
-            "Please Give the following details correctly to generate your Workout Plan"
-        )
-        with open("myworkout.txt", "w") as file:
-            file.write(str(plan_workout()))
+        print("Please provide the following details correctly to generate your Workout Plan")
+        workout_plan = plan_workout()
+        if workout_plan:
+            with open("myworkout.txt", "w") as file:
+                file.write(workout_plan)
+            timelog.append(timestamp)
+            print("Workout plan generated and saved.")
+        else:
+            print("Failed to generate workout plan.")
+        print("--------------------")
 
     elif user_input == 2:
-        print("starting your workout")
-        start_workout()
+        if streak == 0:
+            print("Starting your workout")
+            start_workout()
+            timelog.append(timestamp)
+        
+        elif timestamp not in timelog:
+            print("You have not logged any workout yet.")
+            
+            streak = str(streak + 1)
+            with open("streak.txt", "w") as file:
+                file.write(streak)
+        elif timestamp in timelog:
+            print("You have already done your workout today.")
+
     elif user_input == 3:
-        print("option 3 under construction")
+        print("Option 3 under construction")
+
     elif user_input == 4:
-        print("ok exit")
+        print("Exiting the program. Goodbye!")
         break
     else:
-        print("either there was error in code or your input is wrong")
+        print("Invalid option. Please select a valid option.")
